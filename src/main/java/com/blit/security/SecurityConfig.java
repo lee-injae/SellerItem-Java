e0bfdde8d0e24ce662e4a45ed37c024b4e932005
Injae Lee <ilee.injae@gmail.com>
2024-04-26 05:30:35 -0400

    security depedency added and securityconfig class created

---
+++ src/main/java/com/blit/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.blit.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) 
+			throws Exception {
+		return http.httpBasic(Customizer.withDefaults())
+				.authorizeHttpRequests(
+						c -> c.anyRequest().authenticated())
+				.build();
+	}
+}
